74aeb67c13e575c127523c9ee2011675eb2362ff
Igor Vasylytsya <vasylytsya.igor@gmail.com>
2021-02-03 20:46:43 +0200

    ready redux toolkit

--- src/redux/reducers/phoneActionsTypes.js
+++
@@ -1,9 +0,0 @@
-// const ADD = "phone/add";
-// const DELETE = "phone/delete";
-// const FILTER = "phone/filter";
-
-// export default {
-//   ADD,
-//   DELETE,
-//   FILTER,
-// };
--- src/redux/reducers/phoneReducer.js
+++ src/redux/reducers/phoneReducer.js
@@ -1,27 +1,76 @@
+import { createReducer } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import actionsTypes from "./phoneActionsTypes";
+import actionsTypes from "../actions/phonebookActions";
 
-const items = (state = [], { type, payload }) => {
-  switch (type) {
-    case actionsTypes.ADD:
-      return [...state, payload.phone];
+// ========================= redux toolkit createReducer ========================
 
-    case actionsTypes.DELETE:
-      return state.filter((task) => task.id !== payload);
+const items = createReducer([], {
+  [actionsTypes.addContact]: (state, action) => [
+    ...state,
+    action.payload.phone,
+  ],
+  [actionsTypes.deleteContact]: (state, action) =>
+    state.filter((task) => task.id !== action.payload),
+});
 
-    default:
-      return state;
-  }
-};
-
-const filter = (state = "", { type, payload }) => {
-  switch (type) {
-    case actionsTypes.FILTER:
-      return payload;
-
-    default:
-      return state;
-  }
-};
+const filter = createReducer("", {
+  [actionsTypes.onHandleFilter]: (_, action) => action.payload,
+});
 
 export default combineReducers({ items, filter });
+
+// ========================= redux toolkit====================
+// import { combineReducers } from "redux";
+// import actionsTypes from "../actions/phonebookActions";
+// const items = (state = [], { type, payload }) => {
+//   switch (type) {
+//     case actionsTypes.addContact.type:
+//       return [...state, payload.phone];
+
+//     case actionsTypes.deleteContact.type:
+//       return state.filter((task) => task.id !== payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const filter = (state = "", { type, payload }) => {
+//   switch (type) {
+//     case actionsTypes.onHandleFilter.type:
+//       return payload;
+//     default:
+//       return state;
+//   }
+// };
+
+// export default combineReducers({ items, filter });
+
+// ========================= react - redux ========================
+// import { combineReducers } from "redux";
+// import actionsTypes from "./phoneActionsTypes";
+// тут ще були заіпротровані константи (які збережені в попередньому коміті)
+// const items = (state = [], { type, payload }) => {
+//   switch (type) {
+//     case actionsTypes.ADD:
+//       return [...state, payload.phone];
+
+//     case actionsTypes.DELETE:
+//       return state.filter((task) => task.id !== payload);
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const filter = (state = "", { type, payload }) => {
+//   switch (type) {
+//     case actionsTypes.FILTER:
+//       return payload;
+
+//     default:
+//       return state;
+//   }
+// };
+
+// export default combineReducers({ items, filter });
